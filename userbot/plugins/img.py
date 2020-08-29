bbce28099aae57276e4261d1dfe08a7e79dbe7ad
Ziddi-bachcha <69814903+Ziddi-bachcha@users.noreply.github.com>
2020-08-29 13:40:59 +0530

    Add files via upload

--- userbot/plugins/img.py
+++ userbot/plugins/img.py
@@ -1,3 +1,14 @@
+#Copyright (C) 2020 The Sonia Roy company LLC.
+
+
+
+#
+
+#Img Module For Userbots ..... Designed And Developed By Phantom....NFX GANG
+
+# Licensed under theSonia Roy Public License, Version 1.d (the "License");
+
+# you may not use this file except in compliance with the License.
 """Download & Upload Images on Telegram\n
 Syntax: `.img <Name>` or `.img (replied message)`
 \n Upgraded and Google Image Error Fixed
@@ -12,14 +23,14 @@ from userbot.utils import admin_cmd
 
 @borg.on(admin_cmd(pattern="img ?(.*)"))
 async def img_sampler(event):
-    await event.edit("`Processing...`")
+    await event.edit("`Tham jaa horha hai download....bs thoda sabar rakh...`")
     reply = await event.get_reply_message()
     if event.pattern_match.group(1):
         query = event.pattern_match.group(1)
     elif reply:
         query = reply.message
     else:
-    	await event.edit("`um, mind mentioning what I actually need to search for ;_;`")
+    	await event.edit("`aaha. Us msg ke reply me .img command do jisko tumhe google search karna hai samjhe...ghusa kuch khaki khopdi me ? ;_;`")
     	return
         
     lim = findall(r"lim=\d+", query)
@@ -41,7 +52,7 @@ async def img_sampler(event):
     }
 
     # passing the arguments to the function
-    paths = response.download(arguments)
+    paths = response.download (arguments)
     lst = paths[0][query]
     await event.client.send_file(await event.client.get_input_entity(event.chat_id), lst)
     shutil.rmtree(os.path.dirname(os.path.abspath(lst[0])))
